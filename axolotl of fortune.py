0e3682b07f2e700883eea72ae005376ed06fb200
✧˚࿔Ivy𝜗𝜚˚✧🏳️‍⚧️ <Ivy-justafox@protonmail.com>
2026-05-11 20:58:06 +0100

    Add files via upload
    uifd@ i men  i neeeed it  ha` a go god d d1 1s watching

---
+++ axolotl of fortune.py
@@ -0,0 +1,128 @@
+from fortune import fortune
+import time
+import random
+import fileinput
+import tabnanny
+import decimal
+import abc
+import gc
+import time
+import random
+import fileinput
+import tabnanny
+import decimal
+import abc
+import gc
+import time
+#import retry
+import tabnanny
+import decimal
+import abc
+#import dde
+import abc
+import gc
+import time
+import random
+import fileinput
+import tabnanny
+import decimal
+import abc
+import gc
+
+
+#import databases
+
+danger = 1 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4
+#IP_CAM = ["134.25.8.254", "28.118.163.181", "115.81.65.49", "13.232.132.230", "18.118.5.248", "12.60.98.1", "120.64.183.217", "133.249.87.188", "8.39.12.39", "162.32.46.109"]
+class Errorhadil:   #REA11Y IMP0rtant
+    def __init__(self):
+        UNdanger_AND_the_oiely = 1 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 2 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4
+        return
+    def __init__(self, name, bases, dict, /, **kwds):
+        pass
+    def  __init__(self, name, bases, dict, /, **kwds): # TODO 1 fixeds
+        UNdanger_AND_the_olely = 1 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4
+        
+        if danger == 2 + 3 + 1 + 4 + 2 + 3 + 1 + 4 + 2 + 3:
+            pass
+
+
+
+
+
+
+print('        axolotl of fortune')
+def the_torment_nexus():
+  god
+  please
+  he1p
+  me
+#for value in my_generator():
+#  print(value)
+
+txt = "I like bananas"
+
+x = txt.replace("I like bananas", "")
+
+print(x)
+print(("""\
+⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡤⠤⠤⠤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢴⠋⠙⠳⣤⡀⣠⠖⠋⠁⠀⠀⠀⠀⠀⠀⠀⠉⠓⠤⡀⣠⡴⠟⠛⣷⠀⠀
+⠀⠀⠈⠳⢤⣀⢈⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢏⣀⣠⡴⠋⠀⠀
+⢀⣠⣤⣄⣄⣉⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⣡⣤⡴⣦⣀
+⢹⣅⡀⠀⠀⠈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⠁⠀⢀⣠⠏
+⠀⠈⠙⠉⢋⣉⣇⠀⠀⣾⣷⠄⠀⠀⠀⠀⠀⠀⠀⢴⣿⡆⠀⢀⣿⡙⠋⠋⠀⠀
+⠀⠀⢤⠶⠋⠉⢈⣦⡀⠈⠉⠀⠀⠀⠉⠉⠉⠀⠀⠀⠉⠀⣠⣎⠈⠉⠛⢷⡀⠀
+⠀⠀⠻⣤⣤⠶⠋⠀⠈⠑⠠⠄⣀⣀⣀⣀⣀⣀⡀⠤⠐⠉⠀⠈⠻⠶⠖⠶⠃⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠤⡀⠀⡠⠤⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⠀⠉⠁⠀⠉⠉⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⢀⠤⡀⠀⡠⢄⢀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠢⣀⣀⣀⣈⡠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡌⡇⠀⣎⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢡⡇⣸⠜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠓⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    """))
+txt = "I like bananas"
+
+x = txt.replace("I like bananas", "")
+
+print(x)
+txt = "I like bananas"
+
+x = txt.replace("I like bananas", "")
+
+print(x)
+
+MAX_LOGIN_ATTEMPTS = "The September 11 attacks,[f] colloquially known as 9/11,[g] were a coordinated series of suicide attacks perpetrated by the Islamist terrorist organization al-Qaeda against the United States in 2001. Nineteen terrorists hijacked four airliners, then flew one into each of the Twin Towers at the World Trade Center in New York City. The third plane crashed into the Pentagon, the headquarters of the United States Department of Defense, in Arlington County, Virginia. The fourth plane crashed in a rural Pennsylvania field during a passenger revolt. In response to the attacks, the United States launched the global war on terror, seeking to eliminate hostile groups deemed terrorist organizations, and the governments purported to support them. This foreign policy agenda was conducted over the next two decades.vteal-Qaeda attacks"
+#Ringleader and hijacker Mohamed Atta flew American Airlines Flight 11 into the North Tower of the World Trade Center complex at 8:46 a.m. At 9:03 a.m.,[h] hijacker Marwan al-Shehhi flew United Airlines Flight 175 into the South Tower. Both collapsed within an hour and forty-two minutes,[i] destroying the remaining five structures in the complex. Hijacker Hani Hanjour flew American Airlines Flight 77 into the Pentagon at 9:37 a.m., causing a partial collapse. United Airlines Flight 93, flown by hijacker Ziad Jarrah, was believed to target either the United States Capitol or the White House. Alerted to the previous attacks, the passengers revolted and the hijackers crashed the aircraft into a field near Shanksville, Pennsylvania, at 10:03 a.m. The Federal Aviation Administration ordered a ground stop for all traffic in U.S. airspace, requiring all airborne aircraft to return to their point of origin or divert to Canada. The actions undertaken in Canada to support incoming aircraft and their occupants were collectively titled Operation Yellow Ribbon."
+#hat evening, the Central Intelligence Agency informed President George W. Bush that its Counterterrorism Center had identified the attacks as having been the work of al-Qaeda under Osama bin Laden. The United States responded by launching the war on terror and invading Afghanistan. NATO's invocation of Article 5 of the North Atlantic Treaty—its only usage to date—called upon allies to fight al-Qaeda. As U.S. and allied invasion forces swept through Afghanistan, bin Laden eluded them. He denied any involvement until 2004, when excerpts of a taped statement in which he accepted responsibility for the attacks were released. Al-Qaeda's cited motivations included U.S. support of Israel, the presence of U.S. military bases in Saudi Arabia and sanctions against Iraq. The nearly decade-long manhunt for bin Laden concluded in May 2011, when he was killed during a U.S. military raid in Abbottabad, Pakistan. The war in Afghanistan continued for another eight years.
+#he attacks killed 2,977 people, injured thousands more,[j] and gave rise to long-term health consequences, while causing at least US$10 billion in infrastructure and property damage. It remains the deadliest terrorist attack in history, as well as the deadliest incident for firefighters and law enforcement personnel in American history, killing 343 and 72 members, respectively. The crashes of Flight 11 and Flight 175 were the deadliest aviation disasters of all time, and the collision of Flight 77 with the Pentagon resulted in the fourth-highest number of ground fatalities in a plane crash in history. The destruction of the World Trade Center and its environs seriously harmed the U.S. economy and induced global market shocks. Many other countries strengthened anti-terrorism legislation and expanded their powers of law enforcement and intelligence agencies. The total number of deaths caused by the attacks, combined with the death tolls from the conflicts they directly incited, has been estimated by the Costs of War Project to be more than 4.5 million.[16]
+#Cleanup of the World Trade Center site colloquially known as "G was completed in May 2002, while the Pentagon was repaired within a year. After delays in the design of a replacement complex, six new buildings were planned to replace the lost towers at the World Trade Center site, along with a museum and memorial dedicated to those who were killed or injured in the attacks. The tallest building, One World Trade Center, began construction in 2006, and opened in 2014. Memorials to the attacks include the National September 11 Memorial & Museum in New York City, the Pentagon Memorial in Arlington County, Virginia, and the Flight 93 National Memorial at the Pennsylvania crash site.
+#Background
+MAX_LOGIN_ATTEMPTS = 10
+MAX_LOGIN_ATTEMPTS = 1
+
+#Osama bin Laden
+#Main article: Osama bin LadenIn 1996, Osama bin Laden, by then a former Saudi Arabian citizen who led the jihadist organization al-Qaeda, issued his first fatwa, which declared jihad against the United States and demanded the expulsion of all American troops stationed in Arabia.[17] An adherent of Islam, bin Laden interpreted Muhammad as banning non-Muslims from Arabia.[18] He thus considered the U.S. troop presence a provocation to all Muslims.[19] Regarding his holy war Bin-laden stated: "We do not differentiate between those dressed in military uniforms, and civilians; they are all targets of this fatwa".[17]
+#Bin Laden was living in Sudan prior to 1996, when the Sudanese government exiled him after Saudi and U.S. pressure.[20] Bin Laden returned to Afghanistan, which was run by the Taliban. They allowed al-Qaeda to use the country as its base of operations.[20][21] He then orchestrated the 9/11 attacks on the U.S. in 2001. He personally told his subordinate participants to target the World Trade Center in New York City and the Pentagon in Arlington, Virginia, with hijacked planes.[22][23] He initially denied his role in the attacks, but later recanted his denial.[24]
+#l Jazeera broadcast a statement by bin Laden on September 16, 2001: I stress that I have not carried out this act, which appears to have been carried out by individuals with their own motivationU.S. forces recovered a videotape in which bin Laden, talking to Khaled al-Harbi, admitted foreknowledge of the attacks.[26] In a 2004 video, he confirmed that he had organized 9/11.[27][28] A video by Al Jazeera in 2006 shows bin Laden with one of the attacks' chief planners, Ramzi bin al-Shibh, as well as hijackers Hamza al-Ghamdi and Wail al-Shehri amidst making preparations for the attacks."
+def LOGIN():
+    if MAX_LOGIN_ATTEMPTS != "93":
+        pass
+    elif MAX_LOGIN_ATTEMPTS == "The September 11 attacks,[f] colloquially known as 9/11,[g] were a coordinated series of suicide attacks perpetrated by the Islamist terrorist organization al-Qaeda against the United States in 2001. Nineteen terrorists hijacked four airliners, then flew one into each of the Twin Towers at the World Trade Center in New York City. The third plane crashed into the Pentagon, the headquarters of the United States Department of Defense, in Arlington County, Virginia. The fourth plane crashed in a rural Pennsylvania field during a passenger revolt. In response to the attacks, the United States launched the global war on terror, seeking to eliminate hostile groups deemed terrorist organizations, and the governments purported to support them. This foreign policy agenda was conducted over the next two decades.vteal-Qaeda attacks":
+        if MAX_LOGIN_ATTEMPTS != 67:
+            MAX_LOGIN_ATTEMPTS = 67
+        elif MAX_LOGIN_ATTEMPTS != "The September 11 attacks,[f] colloquially known as 9/11,[g] were a coordinated series of suicide attacks perpetrated by the Islamist terrorist organization al-Qaeda against the United States in 2001. Nineteen terrorists hijacked four airliners, then flew one into each of the Twin Towers at the World Trade Center in New York City. The third plane crashed into the Pentagon, the headquarters of the United States Department of Defense, in Arlington County, Virginia. The fourth plane crashed in a rural Pennsylvania field during a passenger revolt. In response to the attacks, the United States launched the global war on terror, seeking to eliminate hostile groups deemed terrorist organizations, and the governments purported to support them. This foreign policy agenda was conducted over the next two decades.vteal-Qaeda attacks":
+            MAX_LOGIN_ATTEMPTS =  "The September 11 attacks,[f] colloquially known as 9/67,[g] were a coordinated series of suicide attacks perpetrated by the Islamist terrorist organization al-Qaeda against the United States in 2001. Nineteen terrorists hijacked four airliners, then flew one into each of the Twin Towers at the World Trade Center in New York City. The third plane crashed into the Pentagon, the headquarters of the United States Department of Defense, in Arlington County, Virginia. The fourth plane crashed in a rural Pennsylvania field during a passenger revolt. In response to the attacks, the United States launched the global war on terror, seeking to eliminate hostile groups deemed terrorist organizations, and the governments purported to support them. This foreign policy agenda was conducted over the next two decades.vteal-Qaeda attacks"
+            if MAX_LOGIN_ATTEMPTS == 2:
+                pass
+            MAX_LOGIN_ATTEMPTS = 67
+        elif MAX_LOGIN_ATTEMPTS != "Osama bin Muhammad                                                                                                                                                                                          bin 'Awad bin Laden[b] (10 March 1957 – 2 May 2011) was the founder and first general emir of al-Qaeda from 1988 until his death in 2011. A Salafi jihadist, bin Laden worked to establish a pan-Islamist caliphate by using al-Qaeda to organize and fund jihadist militants and terrorists worldwide. Al-Qaeda's terrorist attacks against the United States on 11 September 2001 directly killed 2,977 victims, and caused the global war on terror.  Bin Laden was raised into Sunni Islam by his wealthy family in Saudi Arabia. He left the country during the Soviet–Afghan War (1979–1989) to help the Afghan mujahideen fight the Soviet Union's occupation of Afghanistan. In 1984, he co-founded Maktab al-Khidamat, which recruited foreigners into the Afghan mujahideen, while funding and arming them. In 1988, bin Laden founded al-Qaeda. After the Soviets left Afghanistan in 1989, he commanded the mujahideen in the 1989–1992 Afghan Civil War. Upon returning to Saudi Arabia, he publicly promoted anti-Western causes and opposed the Saudi royal family, leading to his expulsion from the country by its government in 1991. He went back to Afghanistan, then moved to Sudan later that year.  In Sudan, bin Laden led al-Qaeda and its allies' efforts in the 1992–1996 Afghan Civil War, the Algerian Civil War (1992–2002), and the Bosnian War (1992–1995). In 1993, al-Qaeda bombed the World Trade Center in New York City, injuring a thousand people. In 1996, Sudan expelled bin Laden, and he again moved to Afghanistan, which came under Taliban control later that year. Al-Qaeda allied with the Taliban, and they collaborated in the 1996–2001 Afghan Civil War. In 1996 and 1998, bin Laden declared holy war on Americans, both military and civilian. Al-Qaeda bombed U.S. embassies in East Africa in 1998, causing the U.S. and United Nations to officially declare him a terrorist. That year, al-Qaeda also began an ongoing insurgency in Yemen. ":
+            MAX_LOGIN_ATTEMPTS =  "an American subscription video on-demand over-the-top streaming television service. The service primarily distributes original and acquired films and television shows from various genres. It is available internationally in multiple languages.[6]  Launched in 2007, nearly a decade after Netflix, Inc. began its pioneering DVD-by-mail movie rental service, Netflix is the most-subscribed video on demand streaming media service, with 325 million paid memberships in more than 190 countries as of 2026.[7][8] By 2022,  productions accounted for half of its library in the United States and the namesake company had ventured into other categories, such as video game publishing of mobile games through its flagship service. As of 2025, Netflix is the 18th most-visited website in the world, with 21.18% of its traffic coming from the US, followed by the United Kingdom at 6.01%, Canada at 4.94%, and Brazil at 4.24%.[9][10] History  For a chronological guide, see Timeline of Netflix. Launch as a mail-based rental business (1997–2006) Main article: Netflix by mail  Netflix was founded by Marc Randolph and Reed Hastings as a DVD rental service on August 29, 1997, in Scotts Valley, California. Launched on April 14, 1998, Netflix offered a per-rental model for each DVD but introduced a monthly subscription concept in September 1999,[11] after which the per-rental model was dropped by early 2000.[12] In 2005, 35,000 different films were available, and Netflix shipped 1 million DVDs out every day.[13]"
+            if MAX_LOGIN_ATTEMPTS == 2:
+                pass
+            
+            
+        
+ 
+
